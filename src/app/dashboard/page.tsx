83139e529d9521cdc496775f65117c8954e4662c
PIEROLS15 <pierodanielllanossanchez@gmail.com>
2025-07-12 11:44:41 -0500

    feat: implementation of the reusable header

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,17 @@
+import Header from "@/components/layout/header"
+
 const Dahboard = () => {
     return (
         <>
-            Hola, estas dentro del dashboard
+            <div className="space-y-6 pt-10">
+                <Header
+                    title="Bienvenido al Sistema"
+                    description="Sistema de recomendación de medicamentos para farmacéuticos"
+                    button={null}
+                />
+            </div >
         </>
-    );
+    )
 }
 
-export default Dahboard;
+export default Dahboard
---
+++ src/components/layout/header.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@/components/ui/button"
+
+interface HeaderProps {
+    title: string
+    description: string
+    iconoButton?: React.ComponentType<React.SVGProps<SVGSVGElement>>
+    textButton?: string
+    button?: React.ReactNode | null
+    onOpenModal?: () => void
+}
+
+const Header = ({
+    title,
+    description,
+    iconoButton: Icon,
+    textButton,
+    button,
+    onOpenModal,
+}: HeaderProps) => {
+
+    return (
+        <div className="flex flex-col space-y-4 sm:flex-row sm:items-center sm:justify-between sm:space-y-0 gap-4">
+            <div>
+                <h1 className="text-3xl font-bold text-gray-900 dark:text-white mb-2">{title}</h1>
+                <p className="text-gray-600 dark:text-gray-300">{description}</p>
+            </div>
+
+            {button !== null && (
+                <Button
+                    className="bg-primary hover:bg-secondary text-white self-start sm:self-auto"
+                    onClick={onOpenModal}
+                >
+                    {Icon && <Icon className="mr-2 h-4 w-4" />}
+                    {textButton}
+                </Button>
+            )}
+        </div>
+    )
+}
+
+export default Header
