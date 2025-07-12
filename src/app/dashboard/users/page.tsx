69eefd969ce98709c7504200847309024c37c274
PIEROLS15 <pierodanielllanossanchez@gmail.com>
2025-07-12 12:37:23 -0500

    feat: implementation of user interface

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,7 +1,20 @@
+import Header from '@/components/layout/header'
+import { UserPlus } from 'lucide-react'
+
 const Users = () => {
     return (
-        <>Vista de Usuarios</>
-    );
+        <>
+            <div className="space-y-6 pt-10">
+                <Header
+                    title="Gestión de Usuarios"
+                    description="Administre los usuarios del sistema"
+                    iconoButton={UserPlus}
+                    textButton="Registrar Usuario"
+                />
+
+            </div>
+        </>
+    )
 }
 
-export default Users;
+export default Users
